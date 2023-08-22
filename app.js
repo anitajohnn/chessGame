0fcc95ef7b0a95a07c238928c7a2289abecfe1d2
anitasjohn <124946953+anitajohnn@users.noreply.github.com>
2023-08-22 13:29:05 -0600

    bug not fixed

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function createBoard(){
         const square = document.createElement('div')
         square.classList.add('square')
         square.innerHTML= startPiece
-        square.firstChil && square.firstChild.setAttribute('draggable', true)
+        square.firstChild && square.firstChild.setAttribute('draggable', true)
         square.setAttribute('square-id', i)
         //square.classList.add('beige')
         const row = Math.floor((63-i)/8)+1  //this follows bedmas laws 
@@ -43,29 +43,3 @@ function createBoard(){
 createBoard();
 
 
-const allSquares = document.querySelectorAll("#gameboard .square")
-
-allSquares.forEach(square => {
-    square.addEventListener('dragstart', dragStart)
-    square.addEventListener('dragover', dragOver)
-    square.addEventListener('drag', dragDrop)
-})
-
-let startPositionId
-let draggedElement
-function dragStart (e){
-    startPositionId = e.target.parentNode.getAttribute('square-id')
-    draggedElement = e.target
-}
-
-function dragOver(e){
-    e.preventDefault()
-    
-}
-
-
-function dragDrop(e){
-    e.stopPropagation()
-    e.target.parentNode.append(draggedElement)
-  
-}
